3be032ef0f9aed34795be575653d6e023035adfe
michał gozdek <michalgozdek12@gmail.com>
2023-04-24 14:46:02 +0200

    add custom hooks

--- src/App.js
+++ src/App.js
@@ -1,92 +1,52 @@
-import Form  from "./Form";
+import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Conteiner from "./Conteiner";
-import { useEffect, useState } from "react";
-
-
+import { useState } from "react";
+import { useTasks } from "./Tasks/useTasks";
 
 function App() {
-
   const [hideDoneTasks, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tasks")) || []);
-
-    useEffect(()=>{
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-    },[tasks]);
-   
 
   const toggleHideDone = () => {
-    setHideDone(hideDoneTasks => !hideDoneTasks);
+    setHideDone((hideDoneTasks) => !hideDoneTasks);
   };
 
-  const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done};
-      }
-
-      return task;
-    }))
-  }
-   
-    const setAllDone = () => {
-      setTasks(tasks => tasks.map(task => ({
-        ...task,
-        done: true,
-      })))
-    };
-
-    const addNewTask = (content) => {
-      setTasks(tasks => [
-        ...tasks,
-        {
-          content,
-          done: false,
-          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-        },
-      ]);
-    };
- 
+  const { tasks, removeTask, addNewTask, toggleTaskDone, setAllDone } =
+    useTasks();
 
   return (
     <Conteiner>
-    <header>
-      <h1>Lista zadań</h1>
-    </header>
-    <Section 
-      title="Dodaj nowe zadanie" 
-      body={<Form addNewTask={addNewTask} />} 
-    />
-    
-    <Section 
-    title="Lista Zadań"
-    body={
-    <Tasks 
-      tasks={tasks} 
-      hideDoneTasks={hideDoneTasks} 
-      removeTask={removeTask}
-      toggleTaskDone={toggleTaskDone}
-    />}
-    extraHeaderContent=
-    {<Buttons 
-     tasks={tasks}
-     hideDoneTasks={hideDoneTasks} 
-     toggleHideDone={toggleHideDone} 
-     setAllDone={setAllDone}
-      />}
-    />
-    
+      <header>
+        <h1>Lista zadań</h1>
+      </header>
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
+
+      <Section
+        title="Lista Zadań"
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDoneTasks={hideDoneTasks}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDoneTasks={hideDoneTasks}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
     </Conteiner>
   );
 }
-
-
 
 export default App;
---
+++ src/Tasks/useTasks.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+const getInitialTaks = () => {
+  const localStorageTasks = localStorage.getItem("tasks");
+
+  return localStorageTasks ? JSON.parse(localStorageTasks) : [];
+};
+
+export const useTasks = () => {
+  const [tasks, setTasks] = useState(getInitialTaks);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const removeTask = (id) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  };
+
+  const addNewTask = (content) => {
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
+  const setAllDone = () => {
+    setTasks((tasks) =>
+      tasks.map((task) => ({
+        ...task,
+        done: true,
+      }))
+    );
+  };
+
+  const toggleTaskDone = (id) => {
+    setTasks((tasks) =>
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, done: !task.done };
+        }
+
+        return task;
+      })
+    );
+  };
+
+  return { tasks, removeTask, toggleTaskDone, addNewTask, setAllDone };
+};
